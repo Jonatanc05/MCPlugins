8f3eb6fb6ec63eaec12dfe3bcdc3528762530ef0
Jonata Novais Cirqueira <Jonatanc0511@gmail.com>
2020-08-14 10:40:15 -0300

    Criar LifeBond - plugin que compartilha dano

---
+++ LifeBond/src/main/java/me/jonatanc05/lifebond/Bond.java
@@ -0,0 +1,53 @@
+package me.jonatanc05.lifebond;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+import java.util.ArrayList;
+
+public class Bond implements Listener {
+	protected String name;
+	protected ArrayList<Player> players;
+
+	public Bond(String name, Player p) {
+		this.name = name;
+		players = new ArrayList<Player>();
+		players.add(p);
+	}
+
+	public ArrayList<Player> getPlayers() { return players; }
+
+	public boolean contains(Player p) { return players.contains(p); }
+
+	public void addPlayer(Player p) { players.add(p); }
+
+	public void removePlayer(Player p) {
+		players.remove(p);
+		if (players.isEmpty()) {
+			Main.bonds.remove(this);
+			HandlerList.unregisterAll(this);
+		}
+	}
+
+	public String getName() { return name; }
+
+	@EventHandler
+	public void onPlayerTakeDamage(EntityDamageEvent e) {
+		Entity damaged = e.getEntity();
+		if (!(damaged instanceof Player) || !players.contains(damaged) || e.getCause() == EntityDamageEvent.DamageCause.CUSTOM) {
+			return;
+		}
+		double dmg = e.getFinalDamage();
+		for (Player player : players) {
+			if(player == damaged) continue;
+			player.damage(dmg);
+			player.sendMessage(ChatColor.DARK_RED + ((Player) damaged).getDisplayName() + " sofreu dano!");
+		}
+	}
+
+}
---
+++ LifeBond/src/main/java/me/jonatanc05/lifebond/Main.java
@@ -0,0 +1,87 @@
+package me.jonatanc05.lifebond;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+
+public final class Main extends JavaPlugin {
+
+	public static ArrayList<Bond> bonds;
+
+	@Override
+	public void onEnable() {
+		bonds = new ArrayList<>();
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(!(sender instanceof Player)) return false;
+
+		// Entrar em vínculo existente
+		if (label.equalsIgnoreCase("entrar")) {
+			if(args.length != 1) {
+				sender.sendMessage(ChatColor.RED + "/entrar <nome do vínculo>");
+				return false;
+			}
+			for(Bond bond : bonds) {
+				if(bond.getName().equalsIgnoreCase(args[0])) {
+					bond.addPlayer((Player) sender);
+					sender.sendMessage(ChatColor.GREEN + "Você foi adicionado ao vínculo '" + bond.getName() + "'");
+					return true;
+				}
+			}
+		}
+
+		// Criar vínculo
+		if (label.equalsIgnoreCase("criar")) {
+			if(args.length != 1) {
+				sender.sendMessage(ChatColor.RED + "/criar <nome do vínculo>");
+				return false;
+			}
+			Bond bond = new Bond(args[0], (Player) sender);
+			bonds.add(bond);
+			getServer().getPluginManager().registerEvents(bond, this);
+			sender.sendMessage(ChatColor.GREEN + "Você criou o vínculo '" + args[0] + "'");
+		}
+
+		// Listar vínculos
+		if (label.equalsIgnoreCase("vinculos")) {
+			String msg = "Vínculos (" + bonds.size() + "): ";
+			for(Bond bond : bonds) {
+				if(bond.getName().equalsIgnoreCase(args[0])) {
+					bond.addPlayer((Player) sender);
+					msg += bond.getName() + ", ";
+					return true;
+				}
+			}
+		}
+
+		// Informa o vínculo do player
+		if (label.equalsIgnoreCase("meuvinculo")) {
+			for(Bond bond : bonds) {
+				if(bond.contains((Player) sender)) {
+					sender.sendMessage("Você pertence ao vínculo .");
+					return true;
+				}
+			}
+			sender.sendMessage("Você não está inserido em nenhum vínculo.");
+			return true;
+		}
+
+		// Retira o player do vínculo atual
+		if (label.equalsIgnoreCase("sair")) {
+			for(Bond bond : bonds) {
+				if(bond.contains((Player) sender)) {
+					bond.removePlayer((Player) sender);
+					return true;
+				}
+			}
+			sender.sendMessage(ChatColor.RED + "Você não está inserido em nenhum vínculo.");
+			return true;
+		}
+		return false;
+	}
+}
